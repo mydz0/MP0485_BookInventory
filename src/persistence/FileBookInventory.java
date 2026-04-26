7c83fa8723ea19701cb3a739ca703f57d80a5d95
mydz0 <stephanyarellano147@gmail.com>
2026-04-26 23:47:26 +0200

    changed parameters to HashMaps

--- src/persistence/FileBookInventory.java
+++ src/persistence/FileBookInventory.java
@@ -7,11 +7,13 @@ package persistence;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import model.Book;
 
 /**
@@ -31,7 +33,7 @@ public class FileBookInventory {
         this.newFile = newFile;
     }
 
-    public static void save(ArrayList<Book> books) {
+    public static void save(HashMap<String, Book> bookInventory) {
         try {
 
             if (!folder.exists()) {
@@ -45,7 +47,7 @@ public class FileBookInventory {
             FileWriter fw = new FileWriter(file, false);
             BufferedWriter bw = new BufferedWriter(fw);
 
-            for (Book b : books) {
+            for (Book b : bookInventory.values()) {
                 bw.write(b.toString());
                 bw.newLine();
                 bw.flush();
@@ -58,8 +60,8 @@ public class FileBookInventory {
 
     }
 
-    public static ArrayList<Book> load() {
-        ArrayList<Book> loadBooks = new ArrayList<>();
+    public static HashMap<String, Book> load() {
+        HashMap<String, Book> loadBooks = new HashMap<>();
         try {
             FileReader fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr);
@@ -70,10 +72,14 @@ public class FileBookInventory {
                 //make another separator for the array authors, in case a book has more than one author
                 ArrayList<String> authors = new ArrayList<>(Arrays.asList(data[2].split(",")));
                 Book b = new Book(data[0], data[1], authors, Double.parseDouble(data[3]), Integer.parseInt(data[4]));
-                loadBooks.add(b);
+                loadBooks.put(data[1], b);
             }
 
             return loadBooks;
+        } catch (FileNotFoundException e) {
+            // El archivo no existe todavía, simplemente retorna lista vacía
+            return loadBooks;
+
         } catch (IOException ex) {
             System.getLogger(FileBookInventory.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
         }
